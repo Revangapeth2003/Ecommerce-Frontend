248bbad74169fc23d8c367e22f4c30d86b645368
Revangapeth2003 <143567879+Revangapeth2003@users.noreply.github.com>
2024-12-08 18:02:38 +0530

    Update Navbar.jsx

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,10 +1,10 @@
-import { FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
+import { FaSearch, FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-md">
-      <div className="container mx-auto px-1 md:px-16 lg:px-24 py-4 flex justify-around items-center">
+      <div className="container mx-1 px-1 md:px-16 lg:px-24 py-4 flex justify-around items-center">
         <Link to={"/"} className="text-lg font-bold mr-1 font-serif">
           Shopify
         </Link>
@@ -13,7 +13,7 @@ const Navbar = () => {
           <input
             type="text"
             placeholder="Search the products"
-            className="border rounded-lg text-gray-100 py-2 px-3 p-2  bg-gray-700  border-gray-400 relative w-72"
+            className="border sm:hidden  rounded-lg text-gray-100 py-2 px-3 p-2 lg:w-72 bg-gray-700  border-gray-400 relative "
           />
           <button type="submit" className="ml-2">
             <FaSearch className="absolute top-3 right-4 text-red-600" />
@@ -28,9 +28,6 @@ const Navbar = () => {
             <button className="hidden md:block mr-2 font-serif">
               SignIn | SignUp
             </button>
-          </Link>
-          <Link to={"/profile"}>
-            <FaUser className="block mx-2 md:hidden " />
           </Link>
         </div>
       </div>
